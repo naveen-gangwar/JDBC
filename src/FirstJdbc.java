9f329417f84595d0ffc37b60bf67ee83ce06de8e
naveen-gangwar <naveengangwar21@gmail.com>
2022-10-13 22:59:37 +0530

    Jdbc dynamic value practice

--- src/FirstJdbc.java
+++ src/FirstJdbc.java
@@ -4,10 +4,12 @@ import java.sql.DriverManager;
 public class FirstJdbc {
     public static void main(String[] args) {
         try {
+//           load the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+//          creating a connection
             String url = "jdbc:mysql://localhost:3306/youtube";
             Connection con = DriverManager.getConnection(url, "root", "root1");
-            con.close();
+//            con.close();
             if (con.isClosed())
                 System.out.println("connection is closed");
             else
