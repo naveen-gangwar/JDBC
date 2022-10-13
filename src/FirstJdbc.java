056d5781b3c66407e939498f25a8a078a2a79989
naveen-gangwar <naveengangwar21@gmail.com>
2022-10-13 20:09:14 +0530

    port is remove

--- src/FirstJdbc.java
+++ src/FirstJdbc.java
@@ -4,7 +4,6 @@ import java.sql.DriverManager;
 public class FirstJdbc {
     public static void main(String[] args) {
         try {
-//            3306 is port name
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/youtube";
             Connection con = DriverManager.getConnection(url, "root", "root1");
