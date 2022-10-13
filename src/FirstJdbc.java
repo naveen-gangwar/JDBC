70b77e66a188c97110d8a9bbcbeae70a0e8384f1
naveen-gangwar <naveengangwar21@gmail.com>
2022-10-13 16:46:12 +0530

    Initial commit

---
+++ src/FirstJdbc.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class FirstJdbc {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/youtube";
+            Connection con = DriverManager.getConnection(url, "root", "root1");
+            con.close();
+            if (con.isClosed())
+                System.out.println("connection is closed");
+            else
+                System.out.println("connection created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
